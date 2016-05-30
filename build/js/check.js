02b37f11c849b1f8462af9b4538f6da5f0758d39
gu-dron <gu-dron@yandex.ru>
2016-05-30 14:04:34 +0300

    Подправил недочеты отмеченные наставником.
    Попытался применить Array.reduce, не понял как применить к двум
    массивам.

--- build/js/check.js
+++ build/js/check.js
@@ -1,28 +1,26 @@
-function getMessage(a,b) {
-	if (typeof a === 'boolean') {
-		if(a === true) {
-		return ('Я попал в ' + [b]);	
-		}
-		else {
-			return ('Я никуда не попал');
-		}
-	}
-	else if (typeof a === 'number') {
-		return ('Я прыгнул на ' + [a] * 100 + ' сантиметров');
-	}
-	else if (typeof (a) === 'object' && typeof (b) === 'object') {
+function getMessage(a, b) {
+    "use strict";
+    if (typeof a === 'boolean') {
+		if (a) {
+			return ('Я попал в ' + b);
+		} else { return ('Я никуда не попал'); }
+	} else if (typeof a === 'number') {
+		return ('Я прыгнул на ' + a * 100 + ' сантиметров');
+	} else if (Array.isArray(a) && Array.isArray(b)) {
         var length = 0;
         for (var i = 0; i < a.length; i++) {
           length = length + a[i] * b[i];
         }
-        return 'Я прошёл '+ length + ' метров';
-    }
-    
-    else if (typeof (a) === 'object' && typeof (b) != 'object') {
-        var sum = 0;
-        for (var i = 0; i < a.length; i++) {
-        sum = sum + a[i];
-        }
+            return 'Я прошёл '+ length + ' метров';
+    } else if (Array.isArray(a)) {
+       var sum = a.reduce(function(a, b) {
+                                return a + b
+                                });
         return ('Я прошёл ' + sum + ' шагов');
     }		
-};
+}
+
+ /*var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+        sum = sum + a[i];
+        }*/
