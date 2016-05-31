c6a10e3ad59bdaaaf1cf057d4986ae9fb22bc27e
gu-dron <gu-dron@yandex.ru>
2016-05-31 09:45:57 +0300

    module1-task2
    Вывод подсчета длины шагов, сделал через reduce. Почистил код.
    ?Если длина массива b будет меньше a то вернется NaN.

--- build/js/check.js
+++ build/js/check.js
@@ -1,26 +1,23 @@
-function getMessage(a, b) {
-    "use strict";
-    if (typeof a === 'boolean') {
-		if (a) {
-			return ('Я попал в ' + b);
-		} else { return ('Я никуда не попал'); }
-	} else if (typeof a === 'number') {
-		return ('Я прыгнул на ' + a * 100 + ' сантиметров');
-	} else if (Array.isArray(a) && Array.isArray(b)) {
-        var length = 0;
-        for (var i = 0; i < a.length; i++) {
-          length = length + a[i] * b[i];
-        }
-            return 'Я прошёл '+ length + ' метров';
-    } else if (Array.isArray(a)) {
-       var sum = a.reduce(function(a, b) {
-                                return a + b
-                                });
-        return ('Я прошёл ' + sum + ' шагов');
-    }		
-}
+'use strict';
 
- /*var sum = 0;
-        for (var i = 0; i < a.length; i++) {
-        sum = sum + a[i];
-        }*/
+function getMessage(a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return ('Я попал в ' + b);
+		} else {
+            return ('Я никуда не попал');
+        }
+	} else if (typeof a === 'number') {
+        return ('Я прыгнул на ' + a * 100 + ' сантиметров');
+	} else if (Array.isArray(a) && Array.isArray(b)) {
+        var length = a.reduceRight(function (pre, current, index) {
+            return pre + (current * b[index]);
+            });
+        return 'Я прошёл ' + length + ' метров';
+    } else if (Array.isArray(a)) {
+       var sum = a.reduce(function(pre, current) {
+           return pre + current;
+           });
+        return ('Я прошёл ' + sum + ' шагов');
+    }
+}
