063ea9ff4c6770b2903c5298b5a3a084cec96002
gu-dron <gu-dron@yandex.ru>
2016-05-31 15:31:28 +0300

    Попытка номер N
    Борюсь с отступами

--- build/js/check.js
+++ build/js/check.js
@@ -3,21 +3,21 @@
 function getMessage(a, b) {
   if (typeof a === 'boolean') {
     if (a) {
-      return ('Я попал в ' + b);
-		} else {
-            return ('Я никуда не попал');
-        }
-	} else if (typeof a === 'number') {
-        return ('Я прыгнул на ' + a * 100 + ' сантиметров');
-	} else if (Array.isArray(a) && Array.isArray(b)) {
-        var length = a.reduceRight(function (pre, current, index) {
-            return pre + (current * b[index]);
-            });
-        return 'Я прошёл ' + length + ' метров';
-    } else if (Array.isArray(a)) {
-       var sum = a.reduce(function(pre, current) {
-           return pre + current;
-           });
-        return ('Я прошёл ' + sum + ' шагов');
+      return 'Я попал в ' + b;
+    } else {
+      return 'Я никуда не попал';
     }
+  } else if (typeof a === 'number') {
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    var length = b.reduce(function (pre, current, index) {
+      return pre + (current * a[index]);
+    });
+    return 'Я прошёл ' + length + ' метров';
+  } else if (Array.isArray(a)) {
+    var sum = a.reduce(function(pre, current) {
+      return pre + current;
+    });
+    return 'Я прошёл ' + sum + ' шагов';
+  }
 }
