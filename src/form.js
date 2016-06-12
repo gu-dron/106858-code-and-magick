b51ecda35e9d324799c30e29e15664aff30af9ad
gu-dron <gu-dron@yandex.ru>
2016-06-12 19:00:40 +0300

    module3-task2
    Добавил пропадание полей, .review-fields.

--- src/form.js
+++ src/form.js
@@ -16,15 +16,41 @@
   };
 
   var reviewName = document.getElementById('review-name');
+  var reviewText = document.getElementById('review-text');
   var button = document.querySelector('.review-submit');
+  var fieldName = document.querySelector('.review-fields-name');
+  var fieldText = document.querySelector('.review-fields-text');
+  var fieldName2 = fieldName.cloneNode(true);
+  var fieldText2 = fieldText.cloneNode(true);
+  var div = document.querySelector('.review-fields');
+  var newField = document.createElement('label');
+
+  newField.innerHTML = '';
+  div.insertBefore(newField, div.children[2]);
 
   button.setAttribute('disabled', 'disabled');
   reviewName.setAttribute('required', 'required');
+  reviewText.setAttribute('required', 'required');
+
+  reviewText.addEventListener('keyup', function(evt) {
+    if (reviewText.validity.valid) {
+      fieldText.remove();
+      fieldText2.remove();
+      evt.preventDefault();
+    } else {
+      newField.parentNode.insertBefore(fieldText2, newField.nextSibling);
+    }
+  }, true);
 
   reviewName.addEventListener('keyup', function(evt) {
     if (reviewName.validity.valid) {
       button.removeAttribute('disabled', 'disabled');
+      fieldName.remove();
+      fieldName2.remove();
       evt.preventDefault();
+    } else {
+      button.setAttribute('disabled', 'disabled');
+      newField.parentNode.insertBefore(fieldName2, newField.previousSibling);
     }
   }, false);
 
