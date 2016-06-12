b3692b42dccef1122b658ef43b4e85d0040cced3
gu-dron <gu-dron@yandex.ru>
2016-06-12 13:55:06 +0300

    module3-task2
    Подправил, для Трэвиса.

--- src/form.js
+++ src/form.js
@@ -15,19 +15,17 @@
     formContainer.classList.add('invisible');
   };
 
-  var form = document.querySelector('.review-form');
   var reviewName = document.getElementById('review-name');
   var button = document.querySelector('.review-submit');
   var fieldName = document.querySelector('.review-fields-name');
-  var fieldText = document.querySelector('.review-fields-text')
-  var reviewMarkTwo = document.getElementById('review-mark-2')
 
   button.setAttribute('disabled', 'disabled');
   reviewName.setAttribute('required', 'required');
 
-  reviewName.addEventListener('keyup', function (event) {
+  reviewName.addEventListener('keyup', function(evt) {
     if (reviewName.validity.valid) {
       button.removeAttribute('disabled', 'disabled');
+      evt.preventDefault();
     }
   }, false);
 
