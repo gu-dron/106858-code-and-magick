a99cf56a5d18f68584a992038bd8f068da254bc3
gu-dron <gu-dron@yandex.ru>
2016-06-23 17:48:54 +0300

    module5-task1
    Убрал загрузку скрипта jsonp из html. создал XMLHttpRequest.

--- src/reviews.js
+++ src/reviews.js
@@ -1,6 +1,7 @@
 'use strict';
-
 (function() {
+/**@constant {string}*/
+
   var refiewFilter = document.querySelector('.reviews-filter');
   refiewFilter.classList.add('invisible');
 
@@ -14,7 +15,7 @@
     elementToClone = templateElement.querySelector('.review');
   }
 
-  window.reviews.map(function(review) {
+  var getReviewElement = function(review) {
     var element = elementToClone.cloneNode(true);
     element.querySelector('.review-text').textContent = review.description;
     var avatar = element.querySelector('.review-author');
@@ -44,6 +45,38 @@
     }, 10000);
 
     return element;
+  };
+
+  /** @param {function(Array.<Object>)} callback */
+  var getReviews = function(callback) {
+    var xhr = new XMLHttpRequest();
+    /** @param {ProgressEvent} */
+    xhr.onreadystatechange = function(evt) {
+      if (this.readyState == 4) {
+        var loadedData = JSON.parse(evt.target.response);
+        elementToClone.classList.remove('reviews-list-loading');
+        callback(loadedData);
+      }
+      if (this.readyState == 2 || this.readyState == 3) {
+        elementToClone.classList.add('reviews-list-loading');
+      }
+      if (this.status == 404) {
+      }
+    };
+
+    xhr.open('GET', REWIEVS_LOAD_URL);
+    xhr.send();
+  };
+/** @param {Array.<Object>} reviews */
+  var renderReviews = function(reviews) {
+    window.reviews.forEach(function(review) {
+      getReviewElement(review, reviewsContainer);
+    });
+  };
+
+  getReviews(function(loadedReviews) {
+    reviews = loadedReviews;
+    renderReviews(reviews);
   });
 
   refiewFilter.classList.remove('invisible');
